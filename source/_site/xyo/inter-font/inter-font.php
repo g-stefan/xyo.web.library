1098007e9aa2c12eca3229b35115a7d09633048b
Grigore Stefan <g_stefan@yahoo.com>
2026-05-28 14:52:51 +0300

    Update

--- source/_site/xyo/inter-font/inter-font.php
+++ source/_site/xyo/inter-font/inter-font.php
@@ -4,7 +4,7 @@
 // SPDX-FileCopyrightText: 2024-2026 Grigore Stefan <g_stefan@yahoo.com>
 // SPDX-License-Identifier: Apache-2.0
 
-namespace XYO;
+namespace XYO\InterFont;
 
 defined("XYO_WEB") or die("Forbidden");
 
